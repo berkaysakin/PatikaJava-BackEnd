acb80117e8eae5d5cbca67559631cf4b55cb3a29
Berkay Sakin <berkaysakin500@gmail.com>
2022-09-13 20:25:17 +0300

    odev18 eklendi

---
+++ src/odev18.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class odev18 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+
+        int total=0, number;
+
+
+        do {
+
+            System.out.print("Bir sayı giriniz : ");
+            number=input.nextInt();
+
+
+                if (number % 2 == 0 && number % 4 == 0) {
+                    total += number;
+
+                }
+
+
+        }while(number%2==0);
+
+        System.out.println(total);
+    }
+}
