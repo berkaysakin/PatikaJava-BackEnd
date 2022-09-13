a8d0747d25869fa7f5a7400c8f97d8260cef5491
Berkay Sakin <berkaysakin500@gmail.com>
2022-09-13 14:00:17 +0300

    odev 16 eklendi

--- src/odev16.java
+++ src/odev16.java
@@ -7,14 +7,21 @@ public class odev16 {
         int year=input.nextInt();
 
         if(year%4==0){
+
             System.out.println(year + " Bir Artık Yıldır");
+
         }else if(year%100==0){
+
             if(year%400==0){
+
                 System.out.println(year + " Bir Artık Yıldır");
+
             }else{
+
                 System.out.println(year + " Bir Artık Yıl Değildir");
             }
         }else{
+
             System.out.println(year + " Bir Artık Yıl Değildir");
         }
     }
