213136204ba143c418d5ebacf3a85f6dd25ddbfd
Berkay Sakin <berkaysakin500@gmail.com>
2022-09-14 15:04:20 +0300

    odev22 eklendi

--- src/odev22.java
+++ src/odev22.java
@@ -3,37 +3,18 @@ public class odev22 {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
 
-        int number,tempNumber,digitNumber=0,digitValue,digitPow=1,result=0   ;
+        int number,digitNumber,totalValue=0;
+        System.out.print("Sayıyı Giriniz : ");
+         number=input.nextInt();
 
-        System.out.print("Sayıyı giriniz : ");
-        number=input.nextInt();
-        tempNumber=number;
+        while(number!=0){
+            digitNumber=number%10;//basmaklara ayrılıyor ayrılıyor
+            totalValue+=digitNumber;
+            number/=10;//bir basamak yok ediyor
 
-        while(tempNumber!=0){
-        tempNumber/=10;
-        digitNumber++;
         }
+        System.out.println("Rakamlar toplamı : "+totalValue);
 
-        tempNumber=number;
-        while(tempNumber!=0){
-
-            digitValue=tempNumber%10;//10 ile bölümünden kalanı alıyoruz basamaklarına ayırıyoruz
-            digitPow=1;//her döngü başladığında önceki döngüden kalan değer sıfıra eşitlenecek
-
-            for(int i=1; i<=digitNumber; i++){
-            digitPow*=digitValue;
-            }
-
-            result+=digitPow;
-            tempNumber/=10;//Burda da sayının bir basamağını siliyoruz
-        }
-
-
-        if (result==number){
-            System.out.println(result+" Sayısı bir Armstrong sayıdır");
-        }else{
-            System.out.println(result+" Sayısı bir Armstrong sayı değildir");
-        }
 
 
     }
