36270a60ea7fb8c577dd06db9f6169746b5525c3
Berkay Sakin <berkaysakin500@gmail.com>
2022-10-18 18:41:46 +0300

    odev33 eklendi

---
+++ src/Odev33.java
@@ -0,0 +1,21 @@
+public class Odev33 {
+    static boolean isPalindrom(int number) {
+        int tempNumber=number,reverseNumber=0,lastNumber;
+        while (tempNumber!=0){
+            lastNumber=tempNumber%10; //lastNumber = tempNumber'ın 10'a bölümünden kalan
+            reverseNumber=(reverseNumber*10)+lastNumber;
+            tempNumber/=10;
+        }
+        if (reverseNumber==number){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(isPalindrom(88));
+
+    }
+}
+}
